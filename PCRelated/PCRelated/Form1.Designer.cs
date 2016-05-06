453a99e1580785de326a96e31273936dda321c37
FezZHD <zhenya535@gmail.com>
2016-05-07 00:38:52 +0300

    creating new project , cause last was not comfortable

---
+++ PCRelated/PCRelated/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace PCRelated
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PropertyDataGrid = new System.Windows.Forms.PropertyGrid();
+            this.RelatedBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PropertyDataGrid
+            // 
+            this.PropertyDataGrid.HelpVisible = false;
+            this.PropertyDataGrid.Location = new System.Drawing.Point(967, 12);
+            this.PropertyDataGrid.Name = "PropertyDataGrid";
+            this.PropertyDataGrid.Size = new System.Drawing.Size(205, 607);
+            this.PropertyDataGrid.TabIndex = 0;
+            // 
+            // RelatedBox
+            // 
+            this.RelatedBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.RelatedBox.FormattingEnabled = true;
+            this.RelatedBox.Items.AddRange(new object[] {
+            "Клавиатура",
+            "Мышь",
+            "Принтер",
+            "МФУ",
+            "Монитор",
+            "Комплект (мышь + клавиатура)"});
+            this.RelatedBox.Location = new System.Drawing.Point(12, 52);
+            this.RelatedBox.Name = "RelatedBox";
+            this.RelatedBox.Size = new System.Drawing.Size(121, 21);
+            this.RelatedBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1184, 631);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RelatedBox);
+            this.Controls.Add(this.PropertyDataGrid);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "PC Related";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.PropertyGrid PropertyDataGrid;
+        private System.Windows.Forms.ComboBox RelatedBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
--- PCRelated/PCRelated/Properties/AssemblyInfo.cs
+++ PCRelated/PCRelated/Properties/AssemblyInfo.cs
@@ -1,8 +1,6 @@
 ﻿using System.Reflection;
-using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Windows;
 
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
@@ -10,9 +8,9 @@ using System.Windows;
 [assembly: AssemblyTitle("PCRelated")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("PCRelated")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,25 +19,8 @@ using System.Windows;
 // COM, задайте атрибуту ComVisible значение TRUE для этого типа.
 [assembly: ComVisible(false)]
 
-//Чтобы начать сборку локализованных приложений, задайте 
-//<UICulture>CultureYouAreCodingWith</UICulture> в файле .csproj
-//внутри <PropertyGroup>.  Например, если используется английский США
-//в своих исходных файлах установите <UICulture> в en-US.  Затем отмените преобразование в комментарий
-//атрибута NeutralResourceLanguage ниже.  Обновите "en-US" в
-//строка внизу для обеспечения соответствия настройки UICulture в файле проекта.
-
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //где расположены словари ресурсов по конкретным тематикам
-                                     //(используется, если ресурс не найден на странице 
-                                     // или в словарях ресурсов приложения)
-    ResourceDictionaryLocation.SourceAssembly //где расположен словарь универсальных ресурсов
-                                              //(используется, если ресурс не найден на странице, 
-                                              // в приложении или в каких-либо словарях ресурсов для конкретной темы)
-)]
-
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("1c3f69c6-6598-4280-9fc7-f0845b6b8b8e")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
