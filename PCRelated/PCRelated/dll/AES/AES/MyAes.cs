4b55b9ec42dbf9fe1a56860ffbe45f5da1275c67
FezZHD <zhenya535@gmail.com>
2016-05-18 00:54:33 +0300

    fixing aes decrypt

--- PCRelated/PCRelated/dll/AES/AES/MyAes.cs
+++ PCRelated/PCRelated/dll/AES/AES/MyAes.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.ComponentModel;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -6,6 +7,8 @@ namespace MyAes
 {
     public class MyAes
     {
+        private byte[] key;
+        private byte[] IV;
       
         public void Crypt(string path , int serializationType)
         {
@@ -15,7 +18,7 @@ namespace MyAes
             {
                 case 1:
                 {
-                    BinaryReader fileReader = new BinaryReader(file, Encoding.Default);
+                    BinaryReader fileReader = new BinaryReader(file, Encoding.ASCII);
                     fileReader.Read(fileBytes, 0, fileBytes.Length);
                     fileReader.Close();
 
@@ -23,10 +26,12 @@ namespace MyAes
                 }
                 default:
                 {
-                    StreamReader textFileReader = new StreamReader(file, Encoding.Default);
+                    StreamReader textFileReader = new StreamReader(file, Encoding.ASCII);
                     string fileString = textFileReader.ReadToEnd();
+                    int a = fileString.Length;
                     textFileReader.Close();
-                    fileBytes = Encoding.Default.GetBytes(fileString);
+                    ASCIIEncoding stringAsciiEncoding = new ASCIIEncoding();
+                    fileBytes = stringAsciiEncoding.GetBytes(fileString);
                     break;
                 }
 
@@ -34,24 +39,19 @@ namespace MyAes
             }
             file.Close();
             MemoryStream stream;
-            using (Aes currentAes = Aes.Create())
-            {
-                currentAes.Key = new byte[] {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
+            stream = new MemoryStream();
+            InitializationKey();
                 using (Aes myAes = Aes.Create())
                 {
-                    myAes.Key = currentAes.Key;
-                    myAes.IV = currentAes.IV;
+                    myAes.Key = key;
+                    myAes.IV = IV;
 
-                    stream = new MemoryStream();
                     ICryptoTransform encryptor = myAes.CreateEncryptor(myAes.Key, myAes.IV);
-                    using (var encrypt = new CryptoStream(stream, encryptor, CryptoStreamMode.Write))
-                    {
+                    var encrypt = new CryptoStream(stream, encryptor, CryptoStreamMode.Write);
                         encrypt.Write(fileBytes, 0, fileBytes.Length);
                         encrypt.FlushFinalBlock();
-                    }
                 }
 
-            }
             FileStream cryptedFile = new FileStream(path + (".aescrypt"), FileMode.OpenOrCreate, FileAccess.Write);
             cryptedFile.Write(stream.ToArray(), 0, stream.ToArray().Length);
             cryptedFile.Close();
@@ -59,30 +59,40 @@ namespace MyAes
         }
 
 
+        private void InitializationKey()
+        {
+            key = new byte[32];
+            IV= new byte[16];
+            for (int i = 0; i < 32; i++)
+            {
+                key[i] = (byte)i;
+                if (i < 16)
+                {
+                    IV[i] = (byte)i;
+                }
+            }
+        }
+
         public void Decrypt(string path , int serializableType)
         {
+            InitializationKey();
             FileStream file = new FileStream(path,FileMode.Open, FileAccess.Read);
             byte[] bytesFile = new byte[file.Length];
             file.Read(bytesFile, 0, bytesFile.Length);
             file.Close();
             MemoryStream stream;
-            using (Aes currentAes = Aes.Create())
+            stream = new MemoryStream();
+            using (Aes myAes = Aes.Create())
             {
-                currentAes.Key = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F }; 
-                using (Aes myAes = Aes.Create())
-                {
-                    myAes.Key = currentAes.Key;
-                    myAes.IV = currentAes.IV;
-                    stream = new MemoryStream();
-                    ICryptoTransform decryptor = myAes.CreateDecryptor(myAes.Key, myAes.IV);
-                    using (var decrypt = new CryptoStream(stream, decryptor, CryptoStreamMode.Write))
-                    {
-                        decrypt.Write(bytesFile, 0 ,bytesFile.Length);
-                        decrypt.FlushFinalBlock();
-                    }
+                myAes.Key = key;
+                myAes.IV = IV;
 
-                }
-            } 
+                ICryptoTransform decryptor = myAes.CreateDecryptor(myAes.Key, myAes.IV);
+                var decrypt = new CryptoStream(stream, decryptor, CryptoStreamMode.Write);
+                decrypt.Write(bytesFile, 0, bytesFile.Length);
+                decrypt.FlushFinalBlock();
+
+            }
             FileStream newFile = new FileStream(path.Remove(path.Length - 9),FileMode.Create,FileAccess.Write);
             switch (serializableType)
             {
@@ -97,7 +107,9 @@ namespace MyAes
                     newFile.Close();
                     FileInfo currentFileInfo = new FileInfo(newFile.Name);
                     StreamWriter streamWriter = currentFileInfo.CreateText();
-                    string newString = Encoding.Default.GetString(stream.ToArray());
+                    ASCIIEncoding stringAsciiEncoding = new ASCIIEncoding();
+                    byte[] testBytes = stream.ToArray();
+                    string newString = stringAsciiEncoding.GetString(stream.ToArray());
                     streamWriter.WriteLine(newString);
                     streamWriter.Close();
                     break;
